80783c5deb7278a89f8890751faa449c4820d30e
Dark-Py-tech <Dark_Py@outlook.com>
2026-05-06 21:43:06 +0200

    f

---
+++ _Python_/Class_2/class_2.py
@@ -0,0 +1,45 @@
+""" print("  comilla simple '' ")
+
+print('  comilla simple " ')
+
+print("  comilla simple '', comilla doble \"  ")
+
+numero1 = 100
+numero2 = 200
+
+cadena = "12345"
+
+print("Tipo de dato de numero1:", type(numero1))
+print("Tipo de dato de numero2:", type(numero2))
+print("Tipo de dato de cadena:", type(cadena))
+
+cadena_como_numero = int(cadena)
+print("Tipo de dato de cadena convertida a número:", type(cadena_como_numero)) """
+
+
+print("     *\n", 
+      "   * *\n", 
+      "  *   *\n", 
+      " *     *\n", 
+      "***   ***\n", 
+      "  *   *\n", 
+      "  *   *\n", 
+      "  *****")
+
+print("    *", 
+      "   * *", 
+      "  *   *", 
+      " *     *", 
+      "***   ***", 
+      "  *   *", 
+      "  *   *", 
+      "  *****", sep="\n")
+
+print("     *    " * 2)
+print("    * *   " * 2)
+print("   *   *  " * 2)
+print("  *     * " * 2)
+print(" ***   ***" * 2)
+print("   *   *  " * 2)
+print("   *   *  " * 2)
+print("   *****  " * 2)
