051d107b2babd64179cdfc54a76874b657b93ada
Dark-Py-tech <Dark_Py@outlook.com>
2026-05-20 18:09:38 +0200

    up5

--- _Python_/Class_5/class_5.py
+++ _Python_/Class_5/class_5.py
@@ -310,18 +310,18 @@ while True:
     anyo = input("Introduce el año para verificar si es Bisiesto: ")
     anyos = anyo.isdigit()
 
-    while (anyos == True):
+    while anyos:
         anyos = False
         anyo = int(anyo)
 
-        if (anyo < 1582 or anyo < 0):
+        if anyo < 1582 or anyo < 0:
             print ("\033[31mAño no pertenece al calendario Gregoriano (1582 en adelante)")
         else:
-            if ((anyo % 4) != 0):
+            if (anyo % 4) != 0:
                 print (f"Año {anyo} \033[33mNo Bisiesto")
-            elif ((anyo % 100) != 0):
+            elif (anyo % 100) != 0:
                 print (f"Año {anyo} \033[31mBisiesto")
-            elif ((anyo % 400) != 0):
+            elif (anyo % 400) != 0:
                 print (f"Año {anyo} \033[33mNo Bisiesto")
             else:
                 print (f"Año {anyo} \033[31mBisiesto")
