6de121b791e6170347edf3da22944b923f47c8a3
Darkbcn <darknets.bcn@gmail.com>
2026-06-05 19:52:53 +0200

    update

--- _Python_/Class_9/Class_9.py
+++ _Python_/Class_9/Class_9.py
@@ -239,14 +239,14 @@
 
 # def liters_100km_to_miles_gallon(liters):
 #     millas = 100 * 1000 / 1609.344
-
+#
 #     return 100 * 3.785411784 / (liters * 1.609344)
-
+#
 # def miles_gallon_to_liters_100km(miles):
 #     km = miles * 1609.344 / 1000 / 100
-
+#
 #     return 100 * 3.785411784 / (miles * 1.609344)
-
+#
 # print(liters_100km_to_miles_gallon(3.9))
 # print(liters_100km_to_miles_gallon(7.5))
 # print(liters_100km_to_miles_gallon(10.))
